9ac919b8a190d8952048d11944b4a4a843413c05
Josef <josef.prinz@inpro.de>
2021-07-20 10:51:18 +0200

    code samples added

---
+++ Samples/src/ElementAttribute.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2021 Josef Prinz. All rights reserved.
+// Licensed under the MIT License, see License.txt in the project root for license information.
+
+using Aml.Engine.AmlObjects;
+using Aml.Engine.CAEX;
+using Aml.Engine.CAEX.Extensions;
+using System.Diagnostics;
+
+namespace Samples
+{
+    internal static class ElementAttribute
+    {
+        /// <summary>
+        /// Adds an attribute to the aml object with a double value.
+        /// </summary>
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddAttributeWithDoubleValue(IObjectWithAttributes amlObject)
+        {
+            // assigns a new named double attribute, note that the attributeDataType property
+            // is set to xs:double automatically
+            var att = amlObject.SetAttributeValue("Length", 280.76);
+            Debug.Assert(att.AttributeDataType == "xs:double");
+        }
+
+        /// <summary>
+        /// Adds an attribute to the aml object, using a standard attribute type.
+        /// </summary>
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddAttributeUsingStandardAttributeType(IObjectWithAttributes amlObject)
+        {
+            // assigns a new attribute from a defined standard attribute type
+            amlObject.AddAttributeTypeReference(AutomationMLBaseAttributeTypeLib.Cardinality, false, true, "cardinality");
+        }
+
+        /// <summary>
+        /// Adds an attribute to the aml object, using the instantiation method provided by attribute type.
+        /// </summary>
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddAttributeUsingInstantiation(IObjectWithAttributes amlObject)
+        {
+            var amlDocument = amlObject.CAEXDocument();
+
+            // alternative to add attribute
+            // 1. Find the attribute type
+            var attributeType = amlDocument.FindByPath(AutomationMLBaseAttributeTypeLib.Direction) as AttributeFamilyType;
+
+            // 2. Create the instance and assign a name
+            var attribute = attributeType.CreateClassInstance();
+            attribute.Name = "direction";
+
+            // 3. Insert the instance
+            amlObject.Attribute.Insert(attribute);
+        }
+    }
+}
---
+++ Samples/src/ElementInterface.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2021 Josef Prinz. All rights reserved.
+// Licensed under the MIT License, see License.txt in the project root for license information.
+
+namespace Samples
+{
+    using Aml.Engine.AmlObjects;
+    using Aml.Engine.CAEX;
+    using Aml.Engine.CAEX.Extensions;
+    using System.Linq;
+
+    internal static class ElementInterface
+    {
+        /// <summary>
+        /// Adds an external interface to the aml object, using a standard interface class.
+        /// </summary> 
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddExternalInterfaceUsingStandardInterfaceClass (IObjectWithExternalInterface amlObject)
+        {
+            // assigns a new external interface, which references the standard PPRConnector interface class
+            amlObject.AddInterfaceClassReference(AutomationMLInterfaceClassLib.PPRConnector, addUnique: false, addInstance: true, name: "connector1");
+        }
+
+        /// <summary>
+        /// Adds an external interface to the aml object, using the instantiation method provided by interface class.
+        /// </summary> 
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddExternalInterfaceUsingInstantiation (IObjectWithExternalInterface amlObject)
+        {
+            var amlDocument = amlObject.CAEXDocument();
+
+            // alternative to add interfaces
+            // 1. Find the interface class
+            var interfaceClass = amlDocument.FindByPath(AutomationMLInterfaceClassLib.PPRConnector) as InterfaceFamilyType;
+            
+            // 2. Create the instance and assign a name
+            var extInterface = interfaceClass.CreateClassInstance();
+            extInterface.Name = "connector2";
+            
+            // 3. Insert the instance
+            amlObject.ExternalInterface.Insert(extInterface);
+        }
+
+
+        /// <summary>
+        /// Adds an external interface to the aml object without referencing an interface class.
+        /// </summary> 
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddExternalInterfaceWithoutClassreference(IObjectWithExternalInterface amlObject)
+        {
+            // alternative a
+            amlObject.ExternalInterface.Append("interfaceWithoutClass1");
+
+            // alternative b
+            amlObject.New_ExternalInterface("interfaceWithoutClass2");
+        }
+
+
+        /// <summary>
+        /// Adds an external interface to the aml object by copying the first interface
+        /// </summary> 
+        /// <param name="amlObject">The aml object.</param>
+        internal static void AddExternalInterfaceAsCopy(IObjectWithExternalInterface amlObject)
+        {
+            // create a copy from the first interface, it is ensured, that the interface ids are unique
+            var copy = amlObject.ExternalInterface.First().Copy() as ExternalInterfaceType;
+
+            // insert the copy
+            amlObject.ExternalInterface.Insert(copy);
+        }
+    }
+}
